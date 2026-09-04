ca2bc2192f438f00ead419b08c9cca72dc1ce445
micromariaeduarda-creator <micromariaeduarda@gmail.com>
2026-09-04 11:55:52 -0300

    Add src/firebaseConfig.js (placeholder)

---
+++ src/firebaseConfig.js
@@ -0,0 +1,3 @@
+const firebaseConfig = { apiKey: "SUA_API_KEY", authDomain: "SEU_PROJETO.firebaseapp.com", projectId: "SEU_PROJETO", storageBucket: "SEU_PROJETO.appspot.com", messagingSenderId: "SENDER_ID", appId: "APP_ID" };
+
+export default firebaseConfig;
